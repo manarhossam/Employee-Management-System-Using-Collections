4456861d5eedbcdd98bcb139a268a0443fcfedeb
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-09-02 14:00:49 +0300

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System_Using_Collections.Models
+{
+     class Employee
+    {
+        public int Id {  get; set; }
+        public string Name {  get; set; }
+        public DateTime HireDate {  get; set; }
+        public double salary {  get; set; }
+        public int DepartmentId {  get; set; }
+
+        public List<string>skills=new List<string>();
+    }
+}
