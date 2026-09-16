ab575a2503e3577f07aaf6485daa014b1379a60d
Manar Hossam <hossammanar222@gmail.com>
2026-09-16 14:57:19 +0300

    Update library management system

--- Services/Company.cs
+++ Services/Company.cs
@@ -1,6 +1,7 @@
 ﻿using Employee_Management_System_Using_Collections.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace Employee_Management_System_Using_Collections.Services
     class Company
     {
         List<Employee> ActiveEmployee = new List<Employee>();
-        Dictionary<int, Department> View = new Dictionary<int, Department>();
+        Dictionary<int, Department> Departments = new Dictionary<int, Department>();
         Queue<Employee> Onboarding = new Queue<Employee>();
         Stack<String> Actionhistory = new Stack<String>();
         HashSet<String> Uniqeskiils = new HashSet<String>();
@@ -18,39 +19,37 @@ namespace Employee_Management_System_Using_Collections.Services
         {
             if (employee is null)
             {
-                Console.WriteLine("Employee donot exist");
-                return;
+                throw new ArgumentNullException(nameof(employee),"not exist");
             }
-            else if (employee.salary < 0) {
-                Console.WriteLine("Salary cannot be negative");
-                return;
+
+
+            else if (employee.Salary < 0)
+            {
+                throw new ArgumentException("Salary cannot be negative.");
             }
             Onboarding.Enqueue(employee);
-            foreach (string skill in employee.skills)
+            foreach (string skill in employee.Skills)
             {
                 Uniqeskiils.Add(skill);
             }
-            Actionhistory.Push(employee.Name);
+            Actionhistory.Push($"Employee {employee.Name} added to onboarding");
         }
         public void AddDepartment(Department department) {
             if (department is null) {
                 throw new ArgumentNullException(nameof(department));
 
             }
-            if (View.ContainsKey(department.ID))
+            if (Departments.ContainsKey(department.ID))
               throw new InvalidOperationException($"Id already {department.ID} exist");
-           View.Add(department.ID, department);
+           Departments.Add(department.ID, department);
             
         }
         public Employee? seachemployee(int id)
         {
             foreach (Employee employee in ActiveEmployee)
             {
-                if (employee is null)
-                {
-                    throw new ArgumentNullException(nameof(employee));
-
-                }
+                if (employee is null) continue;
+                
                 if (employee.Id == id)
                 {
                     return employee;
@@ -106,7 +105,7 @@ namespace Employee_Management_System_Using_Collections.Services
         }
         public double AverageSalary()
         {
-            if (ActiveEmployee.Count == 0)
+            if (ActiveEmployee.Count == 0 )
             {
                 return 0;
             }
@@ -115,10 +114,45 @@ namespace Employee_Management_System_Using_Collections.Services
 
             foreach (Employee employee in ActiveEmployee)
             {
-                total += employee.salary;
+                total += employee.Salary;
             }
 
             return total / ActiveEmployee.Count;
+        }
+        public void AddskillstoEmployees(int employeeid, string skill) {
+            if (String.IsNullOrWhiteSpace(skill))
+            {
+                throw new ArgumentException("Skill cannot be empty");
+            }
+            Employee? employee = seachemployee(employeeid);
+            if (employee == null) {
+                throw new ArgumentException($"No employee found{employeeid}");
+            }
+            if (employee.Skills.Contains(skill)) { 
+                employee.Skills.Add(skill);
+            }
+            Uniqeskiils.Add(skill);
+            Actionhistory.Push($"skill{skill} added to {employee.Name}");
+          }
+          public void showemployeebydepartment(int departmentid)
+        {
+            if (!Departments.ContainsKey(departmentid)){
+                throw new ArgumentException($"No depaetment withid{departmentid}");
+            }
+            Department department= Departments[departmentid];
+            Console.WriteLine($"{department.Name}");
+            bool found=false;
+            foreach (Employee employee in ActiveEmployee)
+            {
+                if (employee.DepartmentId == departmentid) {
+                    Console.WriteLine($"{employee.Name}{employee.Id}");
+                    found = true;
+                }
+                if (!found) {
+                    Console.WriteLine("No employees currently in this department.");
+
+                }
+            }
         }
     }
 }
